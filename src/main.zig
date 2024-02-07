8ba936ecfa378bfb9cfdf3e1fbfd2d72e9727098
Javad Asgari Shafique <me@javad.sh>
2024-02-08 00:49:38 +0100

    Formatted src/main.zig

--- src/main.zig
+++ src/main.zig
@@ -37,11 +37,11 @@ pub fn main() !void {
         Window.pollEvents();
         try renderer.drawFrame();
         frames += 1;
-        
+
         const time = std.time.milliTimestamp();
         if (time - lastTime > 1000) {
             lastTime = time;
-            var buffer : [128:0]u8 = undefined;
+            var buffer: [128:0]u8 = undefined;
             const tit = try std.fmt.bufPrint(&buffer, "Sudoku Engine | FPS: {d}", .{frames});
             buffer[tit.len] = 0;
             window.setTitle(tit);
