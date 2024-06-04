e910634a834df879310853e47bf96854b3255560
Freddy <frederik.hedeandersen@gmail.com>
2024-06-05 00:00:07 +0200

    The very beginning of building a sudoku board. Also added light gray as a color in the colors engine thingy.

--- src/sudoku/sudoku-game.zig
+++ src/sudoku/sudoku-game.zig
@@ -6,17 +6,12 @@ const engine = @import("../engine.zig");
 
 //Adds components and objects to the game engine for the sudoku game, from the main function.
 pub fn setupGame(game: *engine.Game, allocator: std.mem.Allocator) !void {
-    //var game = gamePtr;
-
-    const images = game.world.resourcePtr(engine.Assets(engine.Image));
-    const image = try images.add(try engine.Image.load_qoi(allocator, "img.qoi"));
-    const normal = try images.add(try engine.Image.load_qoi(allocator, "norm.qoi"));
-
     const materials = game.world.resourcePtr(engine.Assets(engine.StandardMaterial));
     const mat = try materials.add(engine.StandardMaterial{
-        .color_texture = image,
-        .normal_map = normal,
-        .roughness = 0.9,
+        .color = engine.Color.LIGHT_GRAY,
+        .roughness = 0.5,
+        .subsurface = 0.1,
+        .clearcoat = 0.2,
     });
 
     const meshes = game.world.resourcePtr(engine.Assets(engine.Mesh));
@@ -29,7 +24,6 @@ pub fn setupGame(game: *engine.Game, allocator: std.mem.Allocator) !void {
         .translation = engine.Vec3.init(0.0, -0.2, 0.0),
     });
     try box.addComponent(engine.GlobalTransform{});
-    //try box.addComponent(Rotate{});
 
     mat.increment();
     mesh.increment();
@@ -38,12 +32,13 @@ pub fn setupGame(game: *engine.Game, allocator: std.mem.Allocator) !void {
     try box2.addComponent(mat);
     try box2.addComponent(mesh);
     try box2.addComponent(engine.Transform{
-        .translation = engine.Vec3.init(2.0, 0.2, 0.0),
+        .translation = engine.Vec3.init(2.0, 0.2, 1.0),
     });
     try box2.addComponent(engine.GlobalTransform{});
-    //try box2.addComponent(Rotate{});
 
-    try game.world.set_parent(box2.entity, box.entity);
+    //TODO: add lighting. Something something renderstuff dot engine dot magter_det_ikke
+    //const sky = try game.world.spawn();
+    //try sky.addComponent(engine.RenderPlugin.Sky{});
 
     const camera = try game.world.spawn();
     try camera.addComponent(engine.Camera{});
