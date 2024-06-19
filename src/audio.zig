caf779028eec1ca3c99608475787dacaed7f7c1c
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-06-19 17:55:31 +0200

    Add music

---
+++ src/audio.zig
@@ -0,0 +1,59 @@
+const std = @import("std");
+
+const c = @cImport({
+    @cInclude("miniaudio.h");
+});
+
+const engine = @import("engine.zig");
+
+pub const Audio = struct {
+    engine: c.ma_engine,
+    music: c.ma_sound,
+
+    pub fn deinit(self: *Audio) void {
+        c.ma_sound_uninit(&self.music);
+        c.ma_engine_uninit(&self.engine);
+    }
+};
+
+pub const Plugin = struct {
+    pub fn buildPlugin(self: Plugin, game: *engine.Game) !void {
+        _ = self;
+
+        try game.world.addResource(Audio{
+            .engine = undefined,
+            .music = undefined,
+        });
+
+        const audio = game.world.resourcePtr(Audio);
+
+        var result: c.ma_result = undefined;
+
+        result = c.ma_engine_init(null, &audio.engine);
+
+        if (result != c.MA_SUCCESS) {
+            return std.log.err("Failed to initialize miniaudio engine", .{});
+        }
+
+        result = c.ma_sound_init_from_file(
+            &audio.engine,
+            "summer_rain_rain.flac",
+            c.MA_SOUND_FLAG_STREAM,
+            null,
+            null,
+            &audio.music,
+        );
+
+        if (result != c.MA_SUCCESS) {
+            return std.log.err("Failed to load music file", .{});
+        }
+
+        c.ma_sound_set_looping(&audio.music, c.MA_TRUE);
+
+        result = c.ma_sound_start(&audio.music);
+
+        if (result != c.MA_SUCCESS) {
+            return std.log.err("Failed to start music", .{});
+        }
+    }
+};
--- src/engine/Time.zig
+++ src/engine/Time.zig
@@ -6,12 +6,14 @@ const Time = @This();
 start: std.time.Instant,
 frame: std.time.Instant,
 dt: f32,
+since_start: f32,
 
 pub fn init() !Time {
     return .{
         .start = try std.time.Instant.now(),
         .frame = try std.time.Instant.now(),
         .dt = 0.0,
+        .since_start = 0.0,
     };
 }
 
@@ -20,8 +22,12 @@ pub fn update(self: *Time) !void {
     const dt_ns: f32 = @floatFromInt(now.since(self.frame));
     const dt: f32 = dt_ns / std.time.ns_per_s;
 
+    const since_start_ns: f32 = @floatFromInt(now.since(self.start));
+    const since_start: f32 = since_start_ns / std.time.ns_per_s;
+
     self.dt = dt;
     self.frame = now;
+    self.since_start = since_start;
 }
 
 pub const Plugin = struct {
